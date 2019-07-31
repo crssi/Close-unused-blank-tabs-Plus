7b1db33ab90e730025fa01eeb6b102808be4adaf
crssi <herbert@knavs.net>
2019-07-31 20:56:09 +0200

    Initial submit

---
+++ closeunusedblanktabsplus.js
@@ -0,0 +1,37 @@
+/*
+Close unused blank tabs Plus - A Firefox extension to close blank tabs that are not used
+
+This is a fork of "Close unused blank tabs" created by Dustin Luck.
+I have decided to make a small changes, since the original web extension has problems opening multiple links at once.
+Author of the original web exension is not reacheable.
+
+All credits goes to Dustin Luck.
+
+Original text:
+Copyright (c) 2016 Dustin Luck
+This program is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version.
+This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+You should have received a copy of the GNU General Public License along with this program. If not, see http://www.gnu.org/licenses/.
+Contact the author at http://www.dustinluck.com/contact
+*/
+
+const stateComplete = 'complete'
+const blankTabTitles = ['New Tab', 'Cliqz Tab']
+
+function handleCreated(sourceTab)
+{
+    chrome.tabs.query({windowId: sourceTab.windowId}, function(tabs)
+    {
+        for (var tab of tabs) {
+            if (! tab.active
+                && tab.id !== sourceTab.id
+                && tab.status === stateComplete
+				&& blankTabTitles.includes(tab.title))
+            {
+                chrome.tabs.remove(tab.id);
+            }
+        }
+    });
+}
+
+chrome.tabs.onCreated.addListener(handleCreated);
